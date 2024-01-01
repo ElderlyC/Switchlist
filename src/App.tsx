e89faee9d674f39da4202473892ec98a68f447e0
Corey <corey260596@gmail.com>
2024-01-01 21:25:53 +1000

    setup skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [itemList, setItemList] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setItemList(e.target.value);
+  };
+
+  const handleStart = () => {
+    console.log(itemList);
+    setItemList("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>SwitchList</h1>
+        <p>
+          Enter a list of anything, set the intervals, then start the cycle.
+        </p>
+        <textarea
+          placeholder="Enter list items separated by lines"
+          rows={10}
+          onChange={handleChange}
+          value={itemList}
+        ></textarea>
+        <button onClick={handleStart}>Start!</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
